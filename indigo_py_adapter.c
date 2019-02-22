cd756f80ab7a5d388713776853c90a391db490e0
Tim Axelrod <taxelrod@gmail.com>
2019-02-22 11:35:18 -0700

    temporarily added indigo code to print properties - will replace with xml parser

--- indigo_py_adapter.c
+++ indigo_py_adapter.c
@@ -35,3 +35,92 @@ indigo_client *indigo_build_client(char *client_name, indigo_result (*attach_cb)
   indigo_attach_client(&client);
   return &client;
 }
+
+/*  This code extracted from indigo_prop_tool.c as temporary standin for later python code */
+
+static bool print_verbose = false;
+static bool change_requested = false;
+
+void print_property_string(indigo_property *property, const char *message) {
+	indigo_item *item;
+	int i;
+	if (print_verbose && !change_requested) {
+		char perm_str[3] = "";
+		switch(property->perm) {
+		case INDIGO_RW_PERM:
+			strcpy(perm_str, "RW");
+			break;
+		case INDIGO_RO_PERM:
+			strcpy(perm_str, "RO");
+			break;
+		case INDIGO_WO_PERM:
+			strcpy(perm_str, "WO");
+			break;
+		}
+
+		char type_str[20] = "";
+		switch(property->type) {
+		case INDIGO_TEXT_VECTOR:
+			strcpy(type_str, "TEXT_VECTOR");
+			break;
+		case INDIGO_NUMBER_VECTOR:
+			strcpy(type_str, "NUMBER_VECTOR");
+			break;
+		case INDIGO_SWITCH_VECTOR:
+			strcpy(type_str, "SWITCH_VECTOR");
+			break;
+		case INDIGO_LIGHT_VECTOR:
+			strcpy(type_str, "LIGHT_VECTOR");
+			break;
+		case INDIGO_BLOB_VECTOR:
+			strcpy(type_str, "BLOB_VECTOR");
+			break;
+		}
+
+		char state_str[20] = "";
+		switch(property->state) {
+		case INDIGO_IDLE_STATE:
+			strcpy(state_str, "IDLE");
+			break;
+		case INDIGO_ALERT_STATE:
+			strcpy(state_str, "ALERT");
+			break;
+		case INDIGO_OK_STATE:
+			strcpy(state_str, "OK");
+			break;
+		case INDIGO_BUSY_STATE:
+			strcpy(state_str, "BUSY");
+			break;
+		}
+
+		printf("Name : %s.%s (%s, %s)\nState: %s\nGroup: %s\nLabel: %s\n", property->device, property->name, perm_str, type_str, state_str, property->group, property->label);
+		if (message) {
+			printf("Message:\"%s\"\n", message);
+		}
+		printf("Items:\n");
+	}
+
+	for (i = 0; i < property->count; i++) {
+		item = &(property->items[i]);
+		switch (property->type) {
+		case INDIGO_TEXT_VECTOR:
+			printf("%s.%s.%s = \"%s\"\n", property->device, property->name, item->name, item->text.value);
+			break;
+		case INDIGO_NUMBER_VECTOR:
+			printf("%s.%s.%s = %f\n", property->device, property->name, item->name, item->number.value);
+			break;
+		case INDIGO_SWITCH_VECTOR:
+			if (item->sw.value)
+				printf("%s.%s.%s = ON\n", property->device, property->name, item->name);
+			else
+				printf("%s.%s.%s = OFF\n", property->device, property->name, item->name);
+			break;
+		case INDIGO_LIGHT_VECTOR:
+			printf("%s.%s.%s = %d\n", property->device, property->name, item->name, item->light.value);
+			break;
+		case INDIGO_BLOB_VECTOR:
+			break;
+		}
+	}
+	if (print_verbose) printf("\n");
+}
--- propTool.py
+++ propTool.py
@@ -1,9 +1,13 @@
 #!/usr/bin/env python
+
+import sys
+import time
 from _indigo import ffi, lib
 from _indigo.lib import indigo_connect_server
 from _indigo.lib import indigo_disconnect_server
 from _indigo.lib import indigo_start
 from _indigo.lib import indigo_build_client
+from _indigo.lib import print_property_string
 
 @ffi.def_extern()
 def attach_cb(client):
@@ -14,6 +18,9 @@ def attach_cb(client):
 def define_property_cb(client, device, property, message):
     prop = property[0]  # dereference pointer
     print('define_property: ', ffi.string(prop.device), ffi.string(prop.name))
+    print_property_string(property, message)
+#    dev = device[0]  # this is now a device struct
+#    print('device: ', ffi.string(dev.name), dev.version, dev.attach, dev.enumerate_properties)
     return 0
 
 @ffi.def_extern()
@@ -43,17 +50,34 @@ if __name__ == "__main__":
     
     res = indigo_start()
 
-    # build the client with callbacks connected
-    
+    print('start...')
+    if res != lib.INDIGO_OK:
+        print('indigo_start failed', res)
+        sys.exit(res)
+
+    # build the client with callbacks connected; change the signature to return result, pass it a client *
+
+    print('build client')
     client = indigo_build_client(b"propTool", lib.attach_cb, lib.define_property_cb, lib.update_property_cb, lib.delete_property_cb, lib.send_message_cb, lib.detach_cb)
 
     # Connect to the indigo server
 
     serverp = ffi.new("indigo_server_entry * *")  # Note that the indigo_server_entry is serverp[0][0]
-    indigo_connect_server(b"localhost",b"localhost",7624,serverp)
 
+    print('server connect')
+    res = indigo_connect_server(b"localhost",b"localhost",7624,serverp)
+
+    if res != lib.INDIGO_OK:
+        print('indigo_connect_server failed', res)
+        sys.exit(res)
+
+    time.sleep(1)
     # Process args
 
     # Disconnect server
 
-    indigo_disconnect_server(serverp[0])
+    res = indigo_disconnect_server(serverp[0])
+
+    if res != lib.INDIGO_OK:
+        print('indigo_disconnect_server failed', res)
+        sys.exit(res)
