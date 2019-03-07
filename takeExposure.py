ac943acd3aefd6bc8e9261d95b4b3302b2dc1676
Tim <taxelrod@gmail.com>
2019-03-07 09:52:46 -0700

    cleaned up takeExposure.py a little

--- takeExposure.py
+++ takeExposure.py
@@ -37,19 +37,15 @@ if __name__ == "__main__":
     
     # sequence of commands
 
-#    indpy.sendXml("<getProperties version='2.0'>")
-#    time.sleep(5)
-#    indpy.sendXml("<newSwitchVector device='Imager Agent' name='FILTER_CCD_LIST' state='Ok'> <oneSwitch name='None'>Off</oneSwitch> <oneSwitch name='ZWO ASI1600MM Pro #0'>On</oneSwitch></newSwitchVector>")
-#    time.sleep(5)
     indpy.sendXml("<newSwitchVector device='ZWO ASI1600MM Pro #0' name='CONNECTION' state='Ok'> <oneSwitch name='DISCONNECTED'>Off</oneSwitch> <oneSwitch name='CONNECTED'>On</oneSwitch></newSwitchVector>")
     time.sleep(5)
+    
     indpy.sendXml("<newSwitchVector device='ZWO ASI1600MM Pro #0' name='CCD_UPLOAD_MODE' state='Ok'><oneSwitch name='CLIENT'>Off</oneSwitch><oneSwitch name='LOCAL'>On</oneSwitch><oneSwitch name='BOTH'>Off</oneSwitch><oneSwitch name='PREVIEW'>Off</oneSwitch> <oneSwitch name='PREVIEW_LOCAL'>Off</oneSwitch></newSwitchVector>")
-#    indpy.sendXml("<newTextVector device='Imager Agent' name='CCD_LOCAL_MODE'> <oneText name='DIR'>'/home/taxelrod/tmp'</oneText></newTextVector>")
     time.sleep(5)
+    
     indpy.sendXml("<newNumberVector device='ZWO ASI1600MM Pro #0' name='CCD_EXPOSURE'> <oneNumber name='EXPOSURE'>1</oneNumber></newNumberVector>")
+    time.sleep(5)
 
     # Disconnect server
-
-    time.sleep(5)
 
     indpy.stop()
